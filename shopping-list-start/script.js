aadfaf29b4dbd42d2ff1bb22e71aba51a6487705
Aum Patel <77252973+patel-aum-yorku@users.noreply.github.com>
2024-02-05 13:05:47 -0500

    Implemented Remove functionality

--- shopping-list-start/script.js
+++ shopping-list-start/script.js
@@ -1,7 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById('item-list');
-
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 // event listeners' function
 const addItem = (e) => {
     e.preventDefault();
@@ -9,7 +10,7 @@ const addItem = (e) => {
     // validate input
     if(newItem === ''){
         alert('Please add an item');
-    }
+    } else {
     // create list item
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem));
@@ -22,6 +23,10 @@ const addItem = (e) => {
 
     // reset the input field
     itemInput.value = '';
+
+    // showing the filter field and clear button after adding items
+    checkUI();
+}
 }
 
 /**
@@ -50,6 +55,49 @@ const createIcon = (classes) => {
     return i;
 }
 
+/**
+ * This function removes the item from the list when the remove 
+ * button is clicked.
+ * @param {*} event 
+ */
+const removeItem = (event) =>{
+    if (event.target.parentElement.classList.contains('remove-item')){
+        event.target.parentElement.parentElement.remove();
+    }
+    checkUI();
+}
+
+/**
+ * This funciton clears the list on click of the clear button.
+ */
+const clearItem = () => {
+    confirm('Are you sure ?');
+    const items = itemList.querySelectorAll('li');
+    items.forEach(item => {
+        item.remove();
+    })
+    checkUI();
+}
+
+/**
+ * This fucntion checks if the shopping list is empty or not
+ * then proceeds to hide the filter input and clear all button.
+ */
+const checkUI = () => {
+    const newItems = itemList.querySelectorAll('li');
+    if(newItems.length === 0){
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
 
 //event listeners
 itemForm.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
+clearBtn.addEventListener('click',clearItem);
+
+// this fucntion checks when load the page
+checkUI();
